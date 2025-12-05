8dac1ce7cad120b57920c0496437bf2405522472
Raphaël Desprez-Le Goarant <rdesprez@student.42lyon.fr>
2025-12-05 05:23:16 +0100

    Create starter eframe app

---
+++ src/app.rs
@@ -0,0 +1,21 @@
+use eframe::egui;
+
+#[derive(Default)]
+pub struct App {}
+
+impl App {
+    pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &eframe::egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("hi world");
+            if ui.button("clicc me").clicked() {
+                println!("wawa");
+            }
+        });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use crate::app::App;
+
+mod app;
+
 fn main() {
-    println!("Hello, world!");
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "guidn",
+        native_options,
+        Box::new(|cc| Ok(Box::new(App::new(cc)))),
+    )
+    .expect("app error thing");
 }
